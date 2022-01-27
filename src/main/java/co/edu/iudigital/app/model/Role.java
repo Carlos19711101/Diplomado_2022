4f565f3370a8bc7ce1af92c1775f4f0c62a028de
carlos velez <carlos.velez@est.iudigital.edu.co>
2022-01-27 15:10:21 -0500

    :sparkles: actualización tabla, roles y properties

--- src/main/java/co/edu/iudigital/app/model/Role.java
+++ src/main/java/co/edu/iudigital/app/model/Role.java
@@ -1,5 +1,76 @@
 package co.edu.iudigital.app.model;
 
-public class Role {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private long id;
+	
+	@Column(nullable = false, length = 45)
+	private String nombre;
+	
+	@Column(nullable = true, length = 45)
+	private String descripcion;
+
+	
+	@ManyToMany(mappedBy = "roles")
+	private List<Usuario> usuarios;
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
 }
